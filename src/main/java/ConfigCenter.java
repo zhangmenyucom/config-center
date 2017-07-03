7747d8bf750626a85a12a63e99fbafaf3767fca2
xiaolu.zhang <516195940@qq.com>
2017-07-03 10:59:38 +0800

    更新

---
+++ src/main/java/ConfigCenter.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xiaolu.zhang
+ * @desc:配置中心
+ * @date: 2017/7/3 10:12
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigCenter {
+    public static void main(String... args) {
+        new SpringApplicationBuilder(ConfigCenter.class).web(true).run(args);
+    }
+}
