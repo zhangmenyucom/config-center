f102931b4710ccd9c58d14fb5c1fb0a00480f7ab
xiaolu.zhang <516195940@qq.com>
2017-07-04 14:30:01 +0800

    更新

--- src/main/java/com/taylor/ConfigCenter.java
+++ src/main/java/com/taylor/ConfigCenter.java
@@ -1,4 +1,5 @@
-import org.springframework.boot.SpringApplication;
+package com.taylor;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
