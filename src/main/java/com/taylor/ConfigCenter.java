5efef11fac4069e63b7c84b55ee87d90e0643e4c
zhangmenyucom <zhangmenyucom@github.com>
2018-11-04 17:16:03 +0800

    添加eureka client eureka-cleint

--- src/main/java/com/taylor/ConfigCenter.java
+++ src/main/java/com/taylor/ConfigCenter.java
@@ -2,6 +2,7 @@ package com.taylor;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ConfigCenter {
     public static void main(String... args) {
         new SpringApplicationBuilder(ConfigCenter.class).web(true).run(args);
